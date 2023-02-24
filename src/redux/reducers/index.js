ad14896d1cdadaa1a19d759e5a9ef6cc6c97c362
Manas Pasayat <andolasoft.user187@gmail.com>
2023-02-24 10:25:48 +0530

    created reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import { user } from "./user.reducer";
+import { product } from "./product.reducer";
+import { cart } from "./cart.reducer";
 
 const MainReducer = combineReducers({
   user,
+  product,
+  cart,
 });
 
 const rootReducer = (state, action) => {
